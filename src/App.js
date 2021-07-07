8861bf517a71f1e0a3118c0f3b76d05ccd6208e9
Divya Chouhan <divyajchouhan@icloud.com>
2021-07-07 08:07:06 +0100

    data retrieved from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [domain, setDomain] = useState('');
+
+
+  const updateDomain = (event) => {
+    event.preventDefault();
+    setDomain(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchData();
+  }
+
+  const fetchData = () => {
+    fetch(`https://otx.alienvault.com/otxapi/indicator/domain/whois/${domain}`)
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" className="input-box" value={domain} onChange={updateDomain}/>
+        <button type="submit">Get me domain results!</button>
+      </form>
     </div>
   );
 }
